2d6465b51fef70fee03a7c30c3f4b6057223e5bd
SeoeunPark <s2019w29@e-mirim.hs.kr>
2020-11-23 23:26:52 +0900

    jbFunc 비워놓음

--- js/choose.js
+++ js/choose.js
@@ -1,56 +1,34 @@
 function jbFunc1() {
-    document.getElementById('jb1').value = '1';
-    var x;
-    x += 1;
+    
 }
 function jbFunc2() {
-    document.getElementById('jb2').value = '2'; /*진행도 2로 변경*/
-    var x;
-    x += 1;
+    
 }
 function jbFunc3() {
-    document.getElementById('jb2').value = '3';
-    var x;
-    x += 1;
+    
 }
 function jbFunc4() {
-    document.getElementById('jb4').value = '4';
-    var x;
-    x += 1;
+    
 }
 function jbFunc5() {
-    document.getElementById('jb5').value = '5';
-    var x;
-    x += 1;
+    
 }
 function jbFunc6() {
-    document.getElementById('jb6').value = '6';
-    var x;
-    x += 1;
+    
 }
 function jbFunc7() {
-    document.getElementById('jb7').value = '7';
-    var x;
-    x += 1;
+    
 }
 function jbFunc8() {
-    document.getElementById('jb8').value = '8';
-    var x;
-    x += 1;
+    
 }
 function jbFunc9() {
-    document.getElementById('jb9').value = '9';
-    var x;
-    x += 1;
+    
 
 }
 function jbFunc10() {
-    document.getElementById('jb10').value = '10';
-    var x;
-    x += 1;
+    
 }
 function jbFunc11() {
-    document.getElementById('jb11').value = '11';
-    var x;
-    x += 1;
+    
 }
